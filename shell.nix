9774d8545396d7bf3957e8b8d5088268c3f3845e
Amit Aryeh Levy <amit@amitlevy.com>
2026-07-07 13:33:19 -0700

    Nix dependencies update

--- shell.nix
+++ shell.nix
@@ -4,5 +4,7 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.rustup
     pkgs.rustPlatform.bindgenHook
+
+    pkgs.claude-code pkgs.pkg-config pkgs.llhttp pkgs.zlib pkgs.pcre2 pkgs.cmake pkgs.python3 pkgs.openssl
   ];
 }
